8c0f61d9fd0da18004a1ccfeaf2eeb5fce89b0f6
Pallavi-Sharma12 <54979921+Pallavi-Sharma12@users.noreply.github.com>
2021-07-30 20:40:49 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -12,19 +12,20 @@ def handler(event, context):
         if keyName.endswith('.csv'):
             try:
                 csvfile = s3.get_object(Bucket='lambda-csv-event', Key=keyName)
-                contents = csvfile['Body'].read()
-                df = pd.read_csv(io.BytesIO(contents), low_memory=False)
+                csvContent = csvfile['Body'].read()
+                df = pd.read_csv(io.BytesIO(csvContent), low_memory=False)
             except Exception as ex:
                 print("[ERROR] " + str(ex))
                 sns = boto3.client('sns','ap-south-1')
                 response = sns.publish(TopicArn='<SNS Topic ARN>',Message="Hi\n\nMentioned exception has been occurred in handler lambda function. CSV file evaluation process aborts here.\n\n" + str(ex),)
                 
-            to_db = [(row['batch'], row['start'], row['end'], row['records'], row['pass'], row['message']) for index, row in df.iterrows()]
-            cur.executemany("INSERT INTO csvfile (batch, start, end, records, pass, message) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+            toDB_Values = [(row['batch'], row['start'], row['end'], row['records'], row['pass'], row['message']) for index, row in df.iterrows()]
+            cur.executemany("INSERT INTO csvfile (batch, start, end, records, pass, message) VALUES (?, ?, ?, ?, ?, ?);", toDB_Values)
             con.commit()
             con.close()
         else:
             print(f"{keyName} is not a CSV file")
+            
     except Exception as ex:
         print("[ERROR] " + str(ex))
         sns = boto3.client('sns','ap-south-1')
